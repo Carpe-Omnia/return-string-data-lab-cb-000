aa910a8a3f3228079a4df4e848404760db657d67
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-04 03:41:16 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,14 @@ class ProductsController < ApplicationController
     product = Product.find_by(id: params["id"])
     description = product.description
     render plain: description
+  end 
+  def inventory 
+    product = Product.find_by(id: params["id"])
+    inventory = product.inventory
+    if inventory > 0 
+      render plain: "true"
+    else 
+      render plain: "false"
+    end     
+  end   
 end
