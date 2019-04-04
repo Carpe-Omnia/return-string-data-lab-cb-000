2a46b3ba4aa2850f57e718e34a010718cf0636bb
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-04 03:36:12 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,9 @@
 class ProductsController < ApplicationController
-  def index 
-    @products = Product.all 
-  end   
+  def index
+    @products = Product.all
+  end
+  def description
+    product = Product.find_by(id: params["id"])
+    description = product.description
+    render plain: description
 end
